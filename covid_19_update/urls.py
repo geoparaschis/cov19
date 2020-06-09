6cf220afcb140d24304dcd9a34112ea02138a96f
geoparaschis <66213399+geoparaschis@users.noreply.github.com>
2020-06-09 20:36:24 +0300

    Delete urls.py

--- covid_19_update/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-url(r'^$', views.index, name='index'),
-url('news/', views.show_articles, name='news'),
-url('tweets/', views.show_tweets, name='tweets'),
-url('login/', views.login, name='login'),
-]
